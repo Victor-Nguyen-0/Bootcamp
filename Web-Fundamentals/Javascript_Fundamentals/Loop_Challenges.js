3cccdafa9fcf5d378a49925ae6148013882f6f5b
Victor Nguyen <vln3dd@virginia.edu>
2021-11-17 17:06:15 -0500

    Loop Challenges

---
+++ Web-Fundamentals/Javascript_Fundamentals/Loop_Challenges.js
@@ -0,0 +1,38 @@
+// Print odds 1-20
+
+// for(let i=1; i<=20; i++) {
+//     if (i%2==1) {
+//         console.log(i)
+//     }
+// }
+
+// Decreasing multiples of 3
+
+// for(let i=100; i>0; i--) {
+//     if (i%3==0) {
+//         console.log(i)
+//     }
+// }
+
+// Printing sequence
+
+// for(let i=4; i>=-4; i-=1.5) {
+//     console.log(i)
+// }
+
+// Sigma - write code to sum 1-100
+
+// let sum = 0
+// for(let i=1; i<=100; i++) {
+//     sum += i
+// }
+// console.log(sum)
+
+// Factorials - multiply all values from 1-12 
+
+let product = 1
+for(let i=1; i<=12; i++) {
+    product = product * i
+}
+
+console.log(product)
