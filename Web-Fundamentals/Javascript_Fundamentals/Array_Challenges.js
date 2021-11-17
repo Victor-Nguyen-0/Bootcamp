5714c3e99bd9b21bc4c6aa255a7254a6ce29d52f
Victor Nguyen <vln3dd@virginia.edu>
2021-11-17 18:09:48 -0500

    fixed first problem

--- Web-Fundamentals/Javascript_Fundamentals/Array_Challenges.js
+++ Web-Fundamentals/Javascript_Fundamentals/Array_Challenges.js
@@ -1,10 +1,16 @@
 // console yummy if food is given, else console i'm hungry once
+// ***** introduce a counter and then if counter = 0 can console I'm hungry
 
 // function alwaysHungry(arr) {
+//     let foodCount =0
 //     for(let i=0; i<arr.length; i++) {
 //         if (arr[i] == "food") {
 //             console.log("yummy")
+//             foodCount++
 //         }
+//     }
+//     if(foodCount == 0) {
+//         console.log("I'm hungry")
 //     }
 // }
 
