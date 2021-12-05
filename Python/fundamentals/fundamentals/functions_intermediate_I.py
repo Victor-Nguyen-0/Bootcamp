cf797c9ad12a29d9b8cd7fbde6136d7c8512f42d
Victor Nguyen <vln3dd@virginia.edu>
2021-12-05 18:40:21 -0500

    fixed/optimized codes involving dictionaries

--- Python/fundamentals/fundamentals/functions_intermediate_I.py
+++ Python/fundamentals/fundamentals/functions_intermediate_I.py
@@ -38,14 +38,37 @@ def iterateDictionary2(key_name, list):
 # iterateDictionary2('first_name', students)
 # iterateDictionary2('last_name', students)
 
+# def iterate_dictionary2(key_name,list):
+#     for i in range(0, len(list)):
+        
+#         for key,val in list[i].items():
+#             if key == key_name:
+#                 print(val)
+# iterate_dictionary2('first_name',students)
+# iterate_dictionary2('last_name',students)
+
+## alternate way ^^^
+
+
 dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-def printInfo(dict, key_name):
-    print("{} {}" .format(len(dict[key_name]), key_name))
-    print(dict[key_name])
-printInfo(dojo, 'locations')
-printInfo(dojo,'instructors')
+# def printInfo(dict, key_name):
+#     print("{} {}" .format(len(dict[key_name]), key_name))
+#     print(dict[key_name])
+# printInfo(dojo, 'locations')
+# printInfo(dojo,'instructors')
 
 ## come back to last one to not need key name parameter
+
+def print_info(dict):
+    for key,val in dict.items(): # so key,val are the accessible items in a dictionary --> for looping through each key and value pair
+        print("--------------") # separates the location and instructors in the output --> not necessary but makes it look nicer
+        print(f"{len(val)} {key.upper()}") # f string - but can do .format if wanted, length of the value aka the list, and the key's value --> why key.upper? OH BECAUSE THAT'S just for making it all uppercase letters
+        for i in range(0, len(val)): # typical for loop to loop through entire list and print each one
+            print(val[i])
+
+print_info(dojo)
+
+## overall need to get familiar with dictionaries and accessing with key/val b/c it is very unintuitive to me right now
