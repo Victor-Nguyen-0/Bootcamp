211f0fd532dfb732509cc5fe0132031f038bb3c1
Victor Nguyen <vln3dd@virginia.edu>
2022-01-15 16:16:38 -0500

    CRUD practice with UD

--- Python/flask_mysql/crud/users_cr/server.py
+++ Python/flask_mysql/crud/users_cr/server.py
@@ -10,16 +10,15 @@ app.secret_key = 'keep it secret, keep it safe'
 def index():
     return redirect('/users')
 
-@app.route('/users', methods=['GET','POST'])
+@app.route('/users')
 def read_users_all():
-    users = Users.get_all()
-    return render_template("index.html", all_users = users)
+    return render_template("users.html", all_users = Users.get_all_users())
 
 @app.route('/users/new')
 def new():
-    return render_template('create.html')
+    return render_template('create_user.html')
 
-@app.route('/users/create', methods=['GET','POST'])
+@app.route('/users/create', methods=['POST'])
 def create_user():
     data = {
         "first_name": request.form["first_name"],
@@ -27,6 +26,33 @@ def create_user():
         "email": request.form["email"]
     }
     Users.save(data)
+    return redirect('/users')
+
+@app.route('/users/<int:id>')
+def show_user(id):
+    data = {
+        "id": id
+    }
+    return render_template('show_user.html', user = Users.get_one_user(data))
+
+@app.route('/users/<int:id>/edit')
+def edit_user(id):
+    data = {
+        "id": id
+    }
+    return render_template('edit_user.html', user = Users.get_one_user(data))
+
+@app.route('/users/update', methods = ['POST'])
+def update_user():
+    Users.update_entry(request.form)
+    return redirect('/users')
+
+@app.route('/users/<int:id>/delete')
+def delete_user(id):
+    data = {
+        "id": id
+    }
+    Users.delete_entry(data)
     return redirect('/users')
 
 if __name__ == "__main__":
--- Python/flask_mysql/crud/users_cr/users.py
+++ Python/flask_mysql/crud/users_cr/users.py
@@ -10,7 +10,7 @@ class Users:
         self.updated_at = data['updated_at']
 
     @classmethod
-    def get_all(cls):
+    def get_all_users(cls):
         query = "SELECT * FROM users;"
         results = connectToMySQL('users_schema').query_db(query)
         users = []
@@ -21,4 +21,20 @@ class Users:
     @classmethod
     def save(cls, data):
         query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());"
+        return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def get_one_user(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return cls(result[0])
+
+    @classmethod
+    def update_entry(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at = NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query, data)
+
+    @classmethod
+    def delete_entry(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
         return connectToMySQL('users_schema').query_db(query, data)
