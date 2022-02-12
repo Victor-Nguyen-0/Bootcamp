f365f2f45e4eb674cc77dbeb0307f380e1b2b9f2
Victor Nguyen <vln3dd@virginia.edu>
2022-02-12 12:37:46 -0500

    initial jsx practice

---
+++ MERN/React/intro/first_jsx/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello Dojo!
+        </h1>
+        <ul> Things I need to do:
+          <li> Learn React </li>
+          <li> Climb Mt. Everest </li>
+          <li> Run a marathon </li>
+          <li> Feed the dogs </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
