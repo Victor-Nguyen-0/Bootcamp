5365912315373cb025b05afed8d0fc9a01be58ae
Victor Nguyen <vln3dd@virginia.edu>
2022-02-17 21:40:33 -0500

    added css styling

--- MERN/React/functional_components/hook_form/src/components/form.js
+++ MERN/React/functional_components/hook_form/src/components/form.js
@@ -7,47 +7,61 @@ const Form = (props) => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const formDataDivStyle = {
+        textAlign: "left",
+        width: "450px",
+        margin: "auto",
+    }
+
+    const inputDataDivStyle = {
+        borderRadius: "5px",
+        backgroundColor: "#f2f2f2",
+        border: "1px solid darkgrey",
+        padding: "0px 10px",
+        margin: "5px",
+    }
+
     return (
-        <div>
-            <form>
-                <div>
+        <div style={ formDataDivStyle }>
+            <form style={{ marginTop: "20px" }}>
+                <div style={inputDataDivStyle}>
                     <label>
                         First Name:
-                        <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value)} />
+                        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                     </label>
                 </div>
-                <div>
+                <div style={inputDataDivStyle}>
                     <label>
                         Last Name:
-                        <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value)} />
+                        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
                     </label>
                 </div>
-                <div>
+                <div style={inputDataDivStyle}>
                     <label>
                         Email:
-                        <input type="text" value={email} onChange={ (e) => setEmail(e.target.value)} />
+                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </label>
                 </div>
-                <div>
+                <div style={inputDataDivStyle}>
                     <label>
                         Password:
-                        <input type="password" value={password} onChange={ (e) => setPassword(e.target.value)} />
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </label>
                 </div>
-                <div>
+                <div style={inputDataDivStyle}>
                     <label>
                         Confirm Password:
-                        <input type="password" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value)} />
+                        <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
                     </label>
                 </div>
             </form>
             <div>
-            <h2>Your Form Data</h2>
-            <p>First Name: {firstName}</p>
-            <p>Last Name: {lastName}</p>
-            <p>Email: {email}</p>
-            <p>Password: {password}</p>
-            <p>Confirm Password: {confirmPassword}</p>
+                <h2>Your Form Data</h2>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
             </div>
         </div>
     )
