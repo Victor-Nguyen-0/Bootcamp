2c6934a3d6e15f0860b303d1d34caa8142675639
Victor Nguyen <vln3dd@virginia.edu>
2022-03-27 17:13:14 -0400

    unique entry error message works now

--- MERN/Full_Stack_MERN/Authors/server/controllers/author.controller.js
+++ MERN/Full_Stack_MERN/Authors/server/controllers/author.controller.js
@@ -2,14 +2,14 @@ const Author = require("../models/author.model");
 
 module.exports = {
     findAllAuthors: (req, res) => {
-        Author.find({}).collation({locale:'en', strength: 2}).sort({name:1})
+        Author.find({}).collation({ locale: 'en', strength: 2 }).sort({ name: 1 })
             .then((allAuthors) => {
                 console.log(allAuthors);
                 res.json(allAuthors);
             })
             .catch((err) => {
                 console.log("findAllAuthors has failed!");
-                res.status(400).json({message: "Something went wrong in findAllAuthors!", error: err})
+                res.status(400).json({ message: "Something went wrong in findAllAuthors!", error: err })
             })
     },
 
@@ -21,45 +21,52 @@ module.exports = {
             })
             .catch((err) => {
                 console.log("Something went wrong in createNewAuthor:", err.code);
-                // let message = "";
-                // if (err.code === 11000) {
-                //     message = "Author must be a unique author! This one has already been added."
-                //     console.log("Duplicate");
-                //     return res.status(400).json({errors:message})
-                // }
+                if (err.code === 11000) {
+                    message = "Author must be a unique author! This one has already been added."
+                    console.log("Duplicate");
+                    return res.status(400).json(
+                        {
+                            errors: {
+                                name: {
+                                    message
+                                }
+                            }
+                        }
+                    )
+                }
                 res.status(400).json(err)
             })
     },
 
     findOneAuthor: (req, res) => {
-        Author.findOne({_id: req.params.id})
+        Author.findOne({ _id: req.params.id })
             .then((oneAuthor) => {
                 console.log(oneAuthor);
                 res.json(oneAuthor);
             })
             .catch((err) => {
                 console.log("findOneAuthor has failed!");
-                res.status(400).json({message: "Something went wrong in findOneAuthor.", error: err})
+                res.status(400).json({ message: "Something went wrong in findOneAuthor.", error: err })
             })
     },
 
     deleteOneAuthor: (req, res) => {
-        Author.deleteOne({_id: req.params.id})
+        Author.deleteOne({ _id: req.params.id })
             .then((deletedAuthor) => {
                 console.log(deletedAuthor);
                 res.json(deletedAuthor);
             })
             .catch((err) => {
                 console.log("deleteOneAuthor has failed!");
-                res.json({message: "Something went wrong in deleteOneAuthor.", error: err})
+                res.json({ message: "Something went wrong in deleteOneAuthor.", error: err })
             })
     },
 
     updateAuthor: (req, res) => {
-        Author.findOneAndUpdate({_id: req.params.id},
+        Author.findOneAndUpdate({ _id: req.params.id },
             req.body,
-            {new: true, runValidators: true}
-            )
+            { new: true, runValidators: true }
+        )
             .then((updatedAuthor) => {
                 console.log(updatedAuthor);
                 res.json(updatedAuthor);
