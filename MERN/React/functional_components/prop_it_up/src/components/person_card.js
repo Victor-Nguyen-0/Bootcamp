e9ffec45e096e648e97c1ca1ecc292efdbe824dc
Victor Nguyen <vln3dd@virginia.edu>
2022-03-30 09:58:11 -0400

    converted functional into class components

--- MERN/React/functional_components/prop_it_up/src/components/person_card.js
+++ MERN/React/functional_components/prop_it_up/src/components/person_card.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const PersonCard = (props) => {
-    const {firstName, lastName, age, hairColor} = props
-    return (
-        <div className="card">
+
+class PersonCard extends Component {
+    render() {
+        const {firstName, lastName, age, hairColor} = this.props
+        return (
+            <div className="card">
             <h1>{lastName}, {firstName}</h1>
             <p>Age: {age}</p>
             <p>Hair Color: {hairColor}</p>
         </div>
-    )
+        )
+    }
 }
 
 export default PersonCard;
