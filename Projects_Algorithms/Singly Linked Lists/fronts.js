cfd4b6e0ad25ed0830c32b3a2c1e53c8c8b34430
Victor Nguyen <vln3dd@virginia.edu>
2022-06-19 08:36:35 -0400

    added display

--- Projects_Algorithms/Singly Linked Lists/fronts.js
+++ Projects_Algorithms/Singly Linked Lists/fronts.js
@@ -36,13 +36,27 @@ class SLL {
             return this.head.value;
         }
     }
+
+    display() {
+        let listStr = "";
+        if (this.head == null) {
+            return "The list is empty.";
+        }
+        listStr += this.head.value;
+        let runner = this.head.next;
+        while (runner != null) {
+            listStr += ", " + runner.value;
+            runner = runner.next;
+        }
+        return listStr;
+    }
 }
 
 let mySLL = new SLL();
 
-mySLL.addFront(10)
-mySLL.addFront(5);
-mySLL.addFront(73);
+// mySLL.addFront(10)
+// mySLL.addFront(5);
+// mySLL.addFront(73);
 
 // console.log(mySLL);
 // console.log(mySLL.head.next)
@@ -54,10 +68,17 @@ mySLL.addFront(73);
 // mySLL.removeFront();
 // console.log(mySLL);
 
-console.log(mySLL.front());
-mySLL.removeFront();
-console.log(mySLL.front());
-mySLL.removeFront();
-console.log(mySLL.front());
-mySLL.removeFront();
-console.log(mySLL.front());
+// console.log(mySLL.front());
+// mySLL.removeFront();
+// console.log(mySLL.front());
+// mySLL.removeFront();
+// console.log(mySLL.front());
+// mySLL.removeFront();
+// console.log(mySLL.front());
+
+console.log(mySLL.display());
+mySLL.addFront(76);
+mySLL.addFront(2);
+console.log(mySLL.display());
+mySLL.addFront(11.41);
+console.log(mySLL.display());
